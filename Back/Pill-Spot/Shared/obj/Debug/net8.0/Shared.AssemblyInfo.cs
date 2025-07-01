292ede70239607078aa6f28b42a060ad3609c0f7
Elaraby218 <152742814+Elaraby218@users.noreply.github.com>
2025-07-01 16:21:50 +0300

    run files

--- Back/Pill-Spot/Shared/obj/Debug/net8.0/Shared.AssemblyInfo.cs
+++ Back/Pill-Spot/Shared/obj/Debug/net8.0/Shared.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c726c1ffec62e586a07a241e1f5a9c1a3b79ac4a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fbe390fbe6dc0c2ecf9691d0ed7d303b8803eab3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
